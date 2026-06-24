783e8c32320e31ea008494e82f7053599774bb56
Jeff Boutwell <jboutwell@us.ci.org>
2026-06-23 20:33:03 -0600

    add NuqsAdapter

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter, Manrope } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const manropeHeading = Manrope({
   subsets: ["latin"],
@@ -43,7 +44,9 @@ export default function RootLayout({
         manropeHeading.variable,
       )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </body>
     </html>
   );
 }
