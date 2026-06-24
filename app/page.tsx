0682d54d7f324d536b8bc33e31496be2ef2b198c
Jeff Boutwell <jboutwell@us.ci.org>
2026-06-23 20:13:39 -0600

    adjust width

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex flex-1 w-full max-w-6xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Discogs</h1>
         {releases && <ReleaseList collection={releases} />}
       </main>
